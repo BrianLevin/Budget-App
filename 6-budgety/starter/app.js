96da4d1d3f9f7ae330837747cebf50abe171775b
Brian Levin <bml201095@gmail.com>
2020-07-10 16:51:23 -0400

    created a income and express function constructor for when the user adds a new value to the interface

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -1,6 +1,42 @@
 // budget Controller
 var budgetController = (function () {
-   
+    var Expense = function (id, description, value) {
+
+        this.id = id;
+        this.description = description;
+        this.value = value;
+
+
+    };
+
+    var Income = function (id, description, value) {
+
+        this.id = id;
+        this.description = description;
+        this.value = value;
+
+
+    };
+
+    var allExpenses = [];
+
+    var allIncomes = [];
+
+    var totalExpenses = 0;
+
+    var data = {
+        allItems: {
+            exp: [],
+            inc: []
+
+        },
+        totals: {
+
+            exp: 0,
+            inc: 0
+        }
+
+    }
 
 
 })();
@@ -8,29 +44,29 @@ var budgetController = (function () {
 var UIController = (function () {
 
     var DOMstrings = {
-     inputType: ".add__type",
-     inputDescription: ".add__description",
-     inputValue: ".add__value",
-     inputBtn: ".add__btn"
+        inputType: ".add__type",
+        inputDescription: ".add__description",
+        inputValue: ".add__value",
+        inputBtn: ".add__btn"
 
     }
 
     return {
-         getInput: function() {
-         return {
-             type: document.querySelector(DOMstrings.inputType).value, // will either be inc or express
-             description: document.querySelector(DOMstrings.inputDescription).value,
-             value: document.querySelector(DOMstrings.inputValue).value,
+        getInput: function () {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value, // will either be inc or express
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value,
 
-         };
-         
-        
-    },
-getDOMstrings: function() {
-    return DOMstrings;
-}
+            };
 
-};
+
+        },
+        getDOMstrings: function () {
+            return DOMstrings;
+        }
+
+    };
 
 
 })();
@@ -42,53 +78,53 @@ getDOMstrings: function() {
 
 var controller = (function (budgetCtrl, UICtrl) {
     var DOM = UICtrl.getDOMstrings();
-    var setupEventListeners = function(){
+    var setupEventListeners = function () {
 
         document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
-       
 
 
-   document.addEventListener('keypress', function(event){
 
-    //console.log(event);
+        document.addEventListener('keypress', function (event) {
 
-    if(event.Keycode === 13 || event.which === 13) {
+            //console.log(event);
 
-        ctrlAddItem();
+            if (event.Keycode === 13 || event.which === 13) {
+
+                ctrlAddItem();
+
+            }
+
+        });
+
 
     }
 
-   });
-
-
-    }
 
 
 
+    var ctrlAddItem = function () {
+        //1. get the field input data
 
-    var ctrlAddItem = function(){
-//1. get the field input data
-
-var input = UICtrl.getInput();
+        var input = UICtrl.getInput();
 
 
-//2 add item to the budgetcontroller
+        //2 add item to the budgetcontroller
 
-//3  add item to the UI
+        //3  add item to the UI
 
-//4 calculate the budget
+        //4 calculate the budget
 
-// 5 display the budget in the UI
+        // 5 display the budget in the UI
 
 
     };
 
     return {
 
-        init: function(){
-console.log('application is running');
+        init: function () {
+            console.log('application is running');
 
-setupEventListeners();
+            setupEventListeners();
 
         }
     }
